90cd4d5d4beb9eafdb911e169ce7b73e97845421
(5)Inas Abu_Hameidan <70091431+inas-mohammad@users.noreply.github.com>
2020-11-12 19:21:53 +0200

    Update main-route.js

--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const mainRouter = express.Router();
+const {
+getAllArticles,
+createNewArticle,
+changeArticleTitleById,
+} = require('../controllers/main-controller');
 
-mainRouter.get('/', (req, res) => {
-  res.json('HELLO WORLD');
-});
+mainRouter.get('/articles', getAllArticles);
+
+mainRouter.post('/articles', createNewArticle);
+
+mainRouter.put('/articles/:id/:newTitle', changeArticleTitleById);
+
+mainRouter.put('/articles/:id', changeArticleAuthorById);
+
+
+mainRouter.delete('/articles/:id',deleteArticleById );
+
+mainRouter.delete('/articles',deleteArticleByAuthor );
+
+
+
 
 module.exports = mainRouter;
