3faff0dff9fbabcb641f1fc28cff1aeaade05a39
(5)Inas Abu_Hameidan <70091431+inas-mohammad@users.noreply.github.com>
2020-11-15 00:06:12 +0200

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const mainRouter = require('./routes/main-route');
-const cors = require('cors');
 require('dotenv').config();
 
-const db = require('./db');
+const mainRouter = require('./routes/main-route');
+// const cors = require('cors');
+
+ const db = require('./db');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(mainRouter);
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
+
